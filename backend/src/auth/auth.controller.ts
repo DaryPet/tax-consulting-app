1336e8706aeb79493d1aed579fb6afd0e8283544
DaryPet <d7akkord@gmail.com>
2024-11-05 12:12:29 +0100

    Revert "add cookies for sessions"
    This reverts commit 1c47db57a868d5d15cbeb92ce6d36f65e372a478.

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,34 +1,67 @@
+// import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
+// import { AuthService } from './auth.service';
+
+// @Controller('auth')
+// export class AuthController {
+//   constructor(private readonly authService: AuthService) {}
+
+//   @Post('login')
+//   async login(@Body() body: { username: string; password: string }) {
+//     const user = await this.authService.validateUser(
+//       body.username,
+//       body.password,
+//     );
+//     if (!user) {
+//       throw new UnauthorizedException('Invalid credentials');
+//     }
+//     return this.authService.login(user);
+//   }
+// }
+// File: src/auth/auth.controller.ts
+
+// import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
+// import { AuthService } from './auth.service';
+// import { CreateUserDto } from '../user/dto/create-user.dto'; // Импортируем DTO для пользователя
+
+// @Controller('auth')
+// export class AuthController {
+//   constructor(private readonly authService: AuthService) {}
+
+//   @Post('login')
+//   async login(@Body() body: { username: string; password: string }) {
+//     const user = await this.authService.validateUser(
+//       body.username,
+//       body.password,
+//     );
+//     if (!user) {
+//       throw new UnauthorizedException('Invalid credentials');
+//     }
+//     return this.authService.login(user);
+//   }
+
+//   @Post('register') // Новый эндпоинт для регистрации
+//   async register(@Body() createUserDto: CreateUserDto) {
+//     return this.authService.register(createUserDto);
+//   }
+// }
 import {
   Controller,
   Post,
   Body,
   HttpCode,
   HttpStatus,
-  Res,
   UseGuards,
   UnauthorizedException,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
-import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  //   @Post('login')
-  //   async login(@Body() loginDto: any) {
-  //     const user = await this.authService.validateUser(
-  //       loginDto.username,
-  //       loginDto.password,
-  //     );
-  //     if (!user) {
-  //       throw new UnauthorizedException('Invalid credentials');
-  //     }
-  //     return this.authService.login(user);
-  //   }
   @Post('login')
-  async login(@Body() loginDto: any, @Res() res: Response) {
+  async login(@Body() loginDto: any) {
     const user = await this.authService.validateUser(
       loginDto.username,
       loginDto.password,
@@ -36,63 +69,23 @@ export class AuthController {
     if (!user) {
       throw new UnauthorizedException('Invalid credentials');
     }
-
-    const { access_token, refresh_token } = await this.authService.login(user);
-
-    res
-      .cookie('access_token', access_token, {
-        httpOnly: true,
-        maxAge: 60 * 60 * 1000, // 1 час
-      })
-      .cookie('refresh_token', refresh_token, {
-        httpOnly: true,
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 дней
-      })
-      .send({ message: 'Login successful' });
+    return this.authService.login(user);
   }
-  //   @Post('refresh')
-  //   async refresh(@Body() body: any) {
-  //     const { refreshToken } = body;
-  //     return this.authService.refreshToken(refreshToken);
-  //   }
+
   @Post('refresh')
-  async refresh(@Body() body: any, @Res() res: Response) {
+  async refresh(@Body() body: any) {
     const { refreshToken } = body;
-    const { access_token, refresh_token } =
-      await this.authService.refreshToken(refreshToken);
-
-    res
-      .cookie('access_token', access_token, {
-        httpOnly: true,
-        maxAge: 60 * 60 * 1000, // 1 час
-      })
-      .cookie('refresh_token', refresh_token, {
-        httpOnly: true,
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 дней
-      })
-      .send({ message: 'Token refreshed successfully' });
+    return this.authService.refreshToken(refreshToken);
   }
 
-  //   @UseGuards(JwtAuthGuard)
-  //   @Post('logout')
-  //   @HttpCode(HttpStatus.NO_CONTENT)
-  //   async logout(@Body() body: any) {
-  //     const { refreshToken } = body;
-  //     if (!refreshToken) {
-  //       throw new UnauthorizedException('Refresh token is required');
-  //     }
-  //     await this.authService.logout(refreshToken);
-  //   }
   @UseGuards(JwtAuthGuard)
   @Post('logout')
   @HttpCode(HttpStatus.NO_CONTENT)
-  async logout(@Body() body: any, @Res() res: Response) {
+  async logout(@Body() body: any) {
     const { refreshToken } = body;
+    if (!refreshToken) {
+      throw new UnauthorizedException('Refresh token is required');
+    }
     await this.authService.logout(refreshToken);
-
-    res
-      .clearCookie('access_token')
-      .clearCookie('refresh_token')
-      .send({ message: 'Logout successful' });
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,5 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
-import * as cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -8,7 +7,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
