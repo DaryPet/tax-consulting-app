ee4e762ed08d96d1a1199faa1b6dd4bed0942585
DaryPet <d7akkord@gmail.com>
2024-11-05 22:09:46 +0100

    admin role added

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,68 +1,3 @@
-// import {
-//   Controller,
-//   Post,
-//   Body,
-//   Get,
-//   Param,
-//   Patch,
-//   Delete,
-//   UnauthorizedException,
-// } from '@nestjs/common';
-// import { UserService } from './user.service';
-// import { User } from './entities/user.entity';
-// import { CreateUserDto } from './dto/create-user.dto';
-// import { AuthService } from '../auth/auth.service';
-// import { LoginDto } from './dto/login.dto.js';
-
-// @Controller('users')
-// export class UserController {
-//   constructor(
-//     private readonly userService: UserService,
-//     private readonly authService: AuthService,
-//   ) {}
-
-//   // Эндпоинт для регистрации нового пользователя
-//   @Post('register')
-//   async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
-//     return await this.userService.create(createUserDto);
-//   }
-//   // для логину
-//   @Post('login')
-//   async loginUser(
-//     @Body() loginDto: LoginDto,
-//   ): Promise<{ access_token: string }> {
-//     const user = await this.authService.validateUser(
-//       loginDto.username,
-//       loginDto.password,
-//     );
-//     if (!user) {
-//       throw new UnauthorizedException('Invalid credentials');
-//     }
-//     return this.authService.login(user);
-//   }
-//   // Эндпоинт для получения пользователя по ID
-//   @Get(':id')
-//   async getUserById(@Param('id') id: number): Promise<User> {
-//     return await this.userService.findById(id);
-//   }
-
-//   // Эндпоинт для получения всех пользователей
-//   @Get()
-//   async getAllUsers(): Promise<User[]> {
-//     return await this.userService.findAll();
-//   }
-//   @Patch(':id')
-//   async updateUser(
-//     @Param('id') id: number,
-//     @Body() updateData: Partial<User>,
-//   ): Promise<User> {
-//     return await this.userService.updateUser(id, updateData);
-//   }
-//   @Delete(':id')
-//   async deleteUser(@Param('id') id: number): Promise<void> {
-//     return await this.userService.deleteUser(id);
-//   }
-// }
 import {
   Controller,
   Post,
@@ -71,10 +6,17 @@ import {
   Param,
   Patch,
   Delete,
+  UseGuards,
+  Req,
+  ForbiddenException,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Request } from 'express';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard'; // Защита маршрутов с помощью JWT аутентификации
+import { RolesGuard } from '../auth/guards/roles.guard'; // Импортируем RolesGuard
+import { Role } from '../auth/decorators/roles.decorator'; // Импортируем Role декоратор
 
 @Controller('users')
 export class UserController {
@@ -85,24 +27,72 @@ export class UserController {
     return await this.userService.create(createUserDto);
   }
 
+  // @Get()
+  // async getAllUsers(): Promise<User[]> {
+  //   return await this.userService.findAll();
+  // }
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Role('admin') // Только администраторы могут видеть всех пользователей
   @Get()
   async getAllUsers(): Promise<User[]> {
     return await this.userService.findAll();
   }
 
+  // @Get(':id')
+  // async getUserById(@Param('id') id: number): Promise<User> {
+  //   return await this.userService.findById(id);
+  // }
+  @UseGuards(JwtAuthGuard) // Доступ только авторизованным пользователям
   @Get(':id')
-  async getUserById(@Param('id') id: number): Promise<User> {
+  async getUserById(
+    @Param('id') id: number,
+    @Req() req: Request,
+  ): Promise<User> {
+    const user = req.user;
+
+    // Проверяем, если текущий пользователь - администратор или запрашивает свои данные
+    if (user.role !== 'admin' && user.id !== id) {
+      throw new ForbiddenException(
+        'Access denied. You can only view your own data.',
+      );
+    }
+
     return await this.userService.findById(id);
   }
 
+  // @Patch(':id')
+  // async updateUser(
+  //   @Param('id') id: number,
+  //   @Body() updateData: Partial<User>,
+  // ): Promise<User> {
+  //   return await this.userService.updateUser(id, updateData);
+  // }
+
+  @UseGuards(JwtAuthGuard) // Доступ только авторизованным пользователям
   @Patch(':id')
   async updateUser(
     @Param('id') id: number,
     @Body() updateData: Partial<User>,
+    @Req() req: Request,
   ): Promise<User> {
+    const user = req.user;
+
+    // Проверяем, если текущий пользователь - администратор или запрашивает изменение своих данных
+    if (user.role !== 'admin' && user.id !== id) {
+      throw new ForbiddenException(
+        'Access denied. You can only update your own data.',
+      );
+    }
+
     return await this.userService.updateUser(id, updateData);
   }
 
+  // @Delete(':id')
+  // async deleteUser(@Param('id') id: number): Promise<void> {
+  //   return await this.userService.deleteUser(id);
+  // }
+  @UseGuards(JwtAuthGuard, RolesGuard) // Ограничиваем удаление пользователей
+  @Role('admin') // Только администратор может удалять пользователей
   @Delete(':id')
   async deleteUser(@Param('id') id: number): Promise<void> {
     return await this.userService.deleteUser(id);
