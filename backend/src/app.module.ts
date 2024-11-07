e95b0ba0faa9890dd37db456b616e7c59ebb38dc
DaryPet <d7akkord@gmail.com>
2024-11-07 12:46:57 +0100

    connecting Supabase

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,16 +7,18 @@ import { BookingModule } from './booking/booking.module';
 import { DocumentModule } from './documents/document.module';
 
 console.log('Загрузка AppModule');
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'tax_user',
-      password: '12345',
-      database: 'tax_consulting',
+      url: process.env.DATABASE_URL, // Используем строку подключения из переменных окружения
+      // host: 'localhost',
+      // port: 5432,
+      // username: 'tax_user',
+      // password: '12345',
+      // database: 'tax_consulting',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       autoLoadEntities: true,
       synchronize: true,
