d0c723e19466ca930dbd49bfeb9e7f70f6776b90
DaryPet <d7akkord@gmail.com>
2024-11-12 10:22:05 +0100

    login user & admin with redirect

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -4,6 +4,7 @@ import {
   AUTH_LOGIN_URL,
   AUTH_LOGOUT_URL,
   AUTH_REGISTER_URL,
+  AUTH_ME_URL,
 } from "../config/apiConfig";
 
 // Регистрация пользователя
@@ -32,5 +33,22 @@ export const login = async (username: string, password: string) => {
 // Выход пользователя из системы
 export const logout = async () => {
   const response = await axios.post(AUTH_LOGOUT_URL);
+  return response.data;
+};
+// Получение текущего пользователя (маршрут `/auth/me`)
+export const getCurrentUser = async () => {
+  const response = await axios.get(AUTH_ME_URL, {
+    withCredentials: true, // Передаем куки
+  });
+  return response.data; // Возвращаем текущего пользователя
+};
+
+// Рефреш данных пользователя по токену (если используется)
+export const fetchUserData = async (token: string) => {
+  const response = await axios.get(AUTH_ME_URL, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   return response.data;
 };
