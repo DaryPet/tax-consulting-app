d8fd9f850ddd14628d4abc42d95c12addce977a8
DaryPet <d7akkord@gmail.com>
2024-11-14 09:53:19 +0100

    stiky header + footer

--- frontend/src/pages/UserPage/UserPage.tsx
+++ frontend/src/pages/UserPage/UserPage.tsx
@@ -1,53 +1,23 @@
-// import React from "react";
-// import UserDocuments from "../../components/UserDocuments/UserDocuments";
-// import Booking from "../../components/Booking/Booking";
-// import Testimonials from "../../components/Testimonials/TestimonialList";
-// import styles from "./UserPage.module.css";
-// const UserPage = () => {
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.section}>
-//         <h2 className={styles.sectionTitle}>My Documents</h2>
-//         <UserDocuments />
-//       </div>
-//       <div className={styles.section}>
-//         <h2 className={styles.sectionTitle}>Book consultation</h2>
-//         <Booking />
-//       </div>
-
-//       <div className={styles.section}>
-//         <h2 className={styles.sectionTitle}>Testimonials</h2>
-//         <Testimonials />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default UserPage;
-// src/pages/UserPage/UserPage.tsx
-
 import React from "react";
 import UserDocuments from "../../components/UserDocuments/UserDocuments";
 import Booking from "../../components/Booking/Booking";
-// import Testimonials from "../../components/Testimonials/TestimonialList";
 import { useSelector } from "react-redux";
 import { selectAuthUser } from "../../redux/slices/authSlice";
 import styles from "./UserPage.module.css";
-// import TestimonialForm from "../../components/TestimonialForm/TestimonialForm";
 
 const UserPage: React.FC = () => {
   const user = useSelector(selectAuthUser);
 
   return (
     <div className={styles.container}>
-      <div className={styles.section}>
+      <div className={styles.sectionDocuments}>
         <h2 className={styles.sectionTitle}>My Documents</h2>
         <UserDocuments />
       </div>
 
-      <div className={styles.section}>
+      <div className={styles.sectionBooking}>
         <h2 className={styles.sectionTitle}>Book Consultation</h2>
-        {/* Передаем данные пользователя в компонент Booking для заполнения полей автоматически */}
+
         <Booking
           prefillData={{
             name: user?.name,
@@ -57,10 +27,9 @@ const UserPage: React.FC = () => {
         />
       </div>
 
-      <div className={styles.section}>
+      {/* <div className={styles.section}>
         <h2 className={styles.sectionTitle}>Testimonials</h2>
-        {/* <Testimonials /> */}
-      </div>
+      </div> */}
     </div>
   );
 };
