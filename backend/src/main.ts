a39d621e3e02246cf78a1c3dda39e0b5d94521dd
DaryPet <d7akkord@gmail.com>
2024-11-14 21:15:47 +0100

    add vercel for cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,10 +9,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Настройка CORS
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000', // Оставьте для локальной разработки
+      'https://tax-consulting-app-git-main-darya-petrenkos-projects.vercel.app', // Укажите домен вашего фронтенда на Vercel
+    ],
     credentials: true,
   });
-  app.use(cookieParser());
+  app.use(cookieParser()); // Используем cookie-parser для обработки куки
+  // app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(3001);
 }
 bootstrap();
