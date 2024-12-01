07e865bd37eba87dddfa7ea48261431ea06a11e5
DaryPet <d7akkord@gmail.com>
2024-12-01 14:26:45 +0100

    me1

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -40,8 +40,8 @@ export const store = configureStore({
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
-  // devTools: process.env.NODE_ENV !== "production",
-  devTools: process.env.NODE_ENV !== "development",
+  devTools: process.env.NODE_ENV !== "production",
+  // devTools: process.env.NODE_ENV !== "development",
 });
 export const persistor = persistStore(store);
 
